07f571d63f985886fc552b87a5730ecb5d9087fb
gnoale <guillaume.noale@botify.com>
2019-09-03 18:00:47 +0200

    messing around with swapping

---
+++ swap.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+
+  void swap(void* vp1, void* vp2, int size) 
+  {  
+    int* buffer[size];
+    
+    memcpy(buffer, vp1, size);
+    memcpy(vp1, vp2, size);
+    
+    printf("buffer[0] = %p\n", buffer[0]);
+    memcpy(vp2, buffer, size);
+  }
+
+  int g1 = 8;
+  int g2 = 4;
+
+  printf("g1 before swap = %i\n", g1);
+  printf("g2 before swap = %i\n", g2);
+
+  printf("&g1 = %p\n", &g1);
+  printf("&g2 = %p\n", &g2);
+  
+  swap(&g1, &g2, sizeof(int*));
+
+  printf("-------------------------\n");
+
+  printf("g1 after swap = %i\n", g1);
+  printf("g2 after swap = %i\n", g2);
+
+  printf("&g1 = %p\n", &g1);
+  printf("&g2 = %p\n", &g2);
+ 
+
+
+
+}
