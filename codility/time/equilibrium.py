1480eb24318d30bc0cade613b1312c8c51274d73
Guillaume <guillaume@noale.cc>
2020-01-19 14:21:29 +0100

    84%, O(n)

--- codility/time/equilibrium.py
+++ codility/time/equilibrium.py
@@ -1,27 +1,75 @@
-A = [9,7,2,3,4,9,10,6,5]
+#!/usr/bin/env python
 
-s1 = 0 
-s2 = 0 
-R = 0 
-B = []
 
-def equilibrium(A, B, N, s1, s2, R): 
+def equilibrium(A, N, s1, s2): 
     
-    # finished 
-    if N==1: 
-        return s1-s2
+    B = A[0:N//2] 
+    C = A[N//2:N]
 
-    A = A[0:N//2-1] 
-    B = A[N//2:N] 
-    i = N//2-1 
-    j = N//2 
+    N = len(B)
+    M = len(C)
 
+    i = N-1
+    j = 0
+    print(B, C)
+    
     # first iteration 
     if s1 == 0 and s2 == 0: 
-        while i > 0: 
-            s1 += A[i] 
-        while j < N: 
-            s2 += A[i] 
-    
+        for b in B:    
+            s1 += b
+        for c in C:
+            s2 += c
+
+    minimum = abs(s1-s2)
+    print(s1, s2, minimum)
+
+    if abs(s1) > abs(s2):
+        while i >= 0:
+            if abs(s1-s2) < minimum:
+                minimum = abs(s1-s2)
+            s1 -= B[i]
+            s2 += B[i]
+            i -= 1
+        
+    elif abs(s1) < abs(s2):
+        while j < M:
+            if abs(s1-s2) < minimum:
+                minimum = abs(s1-s2)
+            s1 += C[j]
+            s2 -= C[j]
+            j += 1
+
     elif s1 > s2:
-        while
+        while j < M:
+            if abs(s1-s2) < minimum:
+                minimum = abs(s1-s2)
+            s1 += C[j]
+            s2 -= C[j]
+            j += 1
+
+    elif s1 < s2:
+        while i >= 0:
+            if abs(s1-s2) < minimum:
+                minimum = abs(s1-s2)
+            s1 -= B[i]
+            s2 += B[i]
+            i -= 1
+
+    elif s1 == s2:
+        return 0
+
+    return minimum
+    
+
+A = [9,7,2,3,4,9,10,6,5]
+#A = [3,1,2,4,3]
+# return 60 expected 20
+#A = [-10, -20, -30, -40, 100]
+N = len(A)
+s1 = 0 
+s2 = 0 
+
+r = equilibrium(A, N, s1, s2)
+
+print(r)
+
