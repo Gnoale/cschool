549103cc547e8015fa7215711b89088e9e7cf95c
gnoale <guillaume@skynopy.com>
2025-02-19 18:37:38 +0100

    leetcode: refactor btree

--- leetcode/btree/btree_test.go
+++ leetcode/btree/btree_test.go
@@ -6,17 +6,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var maxDepthCases = []struct {
+	root     *TreeNode
+	expected int
+}{
+	{
+		root:     &TreeNode{Val: 3, Left: &TreeNode{Val: 9}, Right: &TreeNode{Val: 20, Left: &TreeNode{Val: 15}, Right: &TreeNode{Val: 7}}},
+		expected: 3,
+	},
+	{
+		root:     &TreeNode{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}},
+		expected: 2,
+	},
+	{
+		root:     nil,
+		expected: 0,
+	},
+}
+
+func TestMaxDepth(t *testing.T) {
+	for _, testCase := range maxDepthCases {
+		assert.Equal(t, testCase.expected, maxDepth(testCase.root))
+	}
+}
+
 var levelOrderCases = []struct {
 	root     *TreeNode
 	expected [][]int
 }{
-	// [3,9,20,null,null,15,7]
 	{root: &TreeNode{Val: 3, Left: &TreeNode{Val: 9}, Right: &TreeNode{Val: 20, Left: &TreeNode{Val: 15}, Right: &TreeNode{Val: 7}}}, expected: [][]int{{3}, {9, 20}, {15, 7}}},
-	// [1]
 	{root: &TreeNode{Val: 1}, expected: [][]int{{1}}},
-	// []
 	{root: nil, expected: [][]int{}},
-	// [1,2,3,4,null,null,5]
 	{root: &TreeNode{Val: 1, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 4}}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 5}}}, expected: [][]int{{1}, {2, 3}, {4, 5}}},
 }
 
